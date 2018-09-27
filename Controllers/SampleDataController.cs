4543b743ce4ae62ce9d4f09685166b81f293099e
Adis Civgin <adis.civgin@upandrunningsoftware.com>
2018-09-27 14:30:49 +0200

    je

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace vega.Controllers
     {
         private static string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Picka Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         [HttpGet("[action]")]
