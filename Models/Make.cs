700635c323fd24a0ff2d523188d3a1b4fb627918
Adis Civgin <adis.civgin@upandrunningsoftware.com>
2018-09-28 11:40:36 +0200

    Apply Constraints on models

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,14 +1,20 @@
 using System.Collections.Generic;
-        
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace vega.Models
 {
+    [Table("Makes")]
     public class Make
     {
         public Make()
         {
             Models = new List<Model>();
         }
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [StringLength(225)]
         public string Name { get; set; }
         
         public IList<Model> Models { get; set; }
--- Models/Model.cs
+++ Models/Model.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace vega.Models
 {
+    [Table("Models")]
     public class Model
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [StringLength(225)]
         public string Name { get; set; }
 
         //Navigation property
